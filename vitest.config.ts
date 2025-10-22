10bb87894cc173c1f39bfdf5139eaecd2e5d327d
Fatih Dogru <ftdogru@yahoo.com>
2025-10-23 00:26:35 +0200

    Add relative path for services folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       "@utils": path.resolve(__dirname, "./src/utils"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@services": path.resolve(__dirname, "./src/services"),
     },
   },
 });
