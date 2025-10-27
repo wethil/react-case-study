d7e24ddbcd70a87ed6b7258dfeaccc36fe626474
Fatih Dogru <ftdogru@yahoo.com>
2025-10-27 18:39:24 +0100

    Disable other tests temporarily

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "yarn dev",
     port: 5173,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
